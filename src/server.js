8bd82adb24fa934fd254411d8a6947612d1f8ba0
lucatobias-pixel <lucas.tobias@aluno.senai.br>
2026-05-14 11:00:48 -0300

    Sprint 5: Frontend inicial integrado com API

--- src/server.js
+++ src/server.js
@@ -1,23 +1,26 @@
+const cors = require('cors'); // 1. Importa primeiro
 const express = require('express');
 const db = require('./database');
 const app = express();
 const PORT = 3000;
 
-app.use(express.json());
+// --- CONFIGURAÇÕES (MIDDLEWARES) ---
+app.use(cors()); // 2. Libera o acesso logo no começo!
+app.use(express.json()); 
+app.use(express.static('public')); // 3. Avisa ao Node para servir a pasta do seu site
 
+// --- ROTAS ---
 app.get('/', (req, res) => {
     res.send('API TechWave Store conectada ao banco de dados!');
 });
 
-app.listen(PORT, () => {
-    console.log(`Servidor rodando em http://localhost:${PORT}`);
-});
-
 const authRoutes = require('./routes/authRoutes');
-
-app.use(express.json()); // Essencial para o Postman funcionar!
-app.use('/auth', authRoutes); // Todas as rotas de auth começarão com /auth
-
+app.use('/auth', authRoutes);
 
 const produtoRoutes = require('./routes/produtoRoutes');
 app.use('/produtos', produtoRoutes);
+
+// --- INICIALIZAÇÃO ---
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
